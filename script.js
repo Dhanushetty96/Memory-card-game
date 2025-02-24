7c9f93a2d50e2f852722df4d17caca0c34a6bf9c
Dhanushetty96 <dhanushetty884@gmail.com>
2025-02-24 20:37:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,130 @@
+const startgamecontainer = document.querySelector(".startgame"),
+      startgamecards = document.querySelectorAll(".startgame .card"),
+      startgame = document.querySelector(".startgame button"),
+      playground = document.querySelector(".playground"),
+      faRepeat = document.querySelector(".fa-repeat");
+
+let levels = 2, columns = 2, rows = 2, matched = 0, cardOne, cardTwo, isPreventClick = true;
+
+startgamecards.forEach((element) => {
+   element.addEventListener("click", (e) => {
+       startgamecards.forEach((el) => {
+           el.classList.remove("active");
+       });
+       e.target.parentElement.classList.add("active");
+       levels = e.target.parentElement.getAttribute("level");
+       columns = e.target.parentElement.getAttribute("column");
+       rows = e.target.parentElement.getAttribute("row");
+
+       console.log(levels, columns, rows);
+   });
+});
+
+startgame.addEventListener("click", (e) => {
+    startgamecontainer.style.display = "none";
+    playground.style.display = "grid";
+
+    playground.style.gridTemplateColumns = `repeat(${columns}, 100px)`;
+    playground.style.gridTemplateRows = `repeat(${rows}, 100px)`;
+    createCards();
+});
+
+function createCards() {
+    const totalCards = columns * rows;
+    const cardArr = [
+        "house",
+        "bomb",
+        "poo",
+        "gift",
+        "egg",
+        "dragon",
+        "person-biking",
+        "jet-fighter-up",
+    ].slice(0, totalCards / 2); // Only use the required number of unique cards
+    shuffleArray(cardArr);
+    shuffleCards([...cardArr.slice(0,levels), ...cardArr.slice(0,levels)]);
+}
+const shuffleArray=(array)=>{
+    for(let i=array.length-1;i>0;i--){
+        const j= Math.floor(Math.random()*(i*1));
+   
+       const temp=array[i];
+       array[i]=array[j];
+       array[j]=temp;
+
+}
+}
+
+function shuffleCards(cards) {
+    playground.innerHTML = "";
+    shuffleArray(cards); 
+    for (let i = 0; i < cards.length; i++) {
+        playground.innerHTML += `
+            <div class="card" onclick='flipCard(this)'>
+                <div class="front"><i class="fas fa-question"></i></div>
+                <div class="back"><i class="fas fa-${cards[i]}"></i></div>
+            </div>
+        `;
+    }
+
+    // Add click event listeners to cards
+ faRepeat.style.display="block";
+   
+}
+function flipCard(card){
+    if(cardOne!=card && isPreventClick){
+
+    
+    card.classList.add('flip');
+    if(!cardOne){
+        cardOne=card;
+        return;
+    }
+    cardTwo=card;
+isPreventClick=false;
+    let cardOneValue=cardOne.querySelector(".back").innerHTML,
+    cardTwoValue=cardTwo.querySelector(".back").innerHTML;
+   matchCards(cardOneValue,cardTwoValue);
+    }
+}
+function matchCards(cardOneValue, cardTwoValue) {
+    if (cardOneValue === cardTwoValue) {
+      
+
+        matched++;
+        if(matched==levels){
+            setTimeout(() => {
+                alert("Congratulations you won !!")
+              }, 500);
+        }
+        cardOne.classList.add("match");
+        cardTwo.classList.add("match");
+      cardOne.removeAttribute("onclick");
+      cardTwo.removeAttribute("onclick");
+
+      cardOne="";
+      cardTwo="";
+      isPreventClick=true;
+      return;
+    }
+    setTimeout(() => {
+      cardOne.classList.add("shake");
+      cardTwo.classList.add("shake");
+    }, 500);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake","flip");
+        cardTwo.classList.remove("shake","flip");
+        cardOne="";
+      cardTwo="";
+       isPreventClick=true;
+      }, 1200);
+  }
+
+faRepeat.addEventListener("click",()=>{
+    startgamecontainer.style.display="grid";
+    playground.style.display="none";
+    faRepeat.style.display="none";
+   ( matched = 0),( cardOne=""),( cardTwo=""),( isPreventClick = true);
+})
+
